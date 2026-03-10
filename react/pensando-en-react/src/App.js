17f1d3e27ead8759e800df9370331720310de000
Javier <javier@eskura21.com>
2026-03-10 19:57:55 +0100

    Maqueta de un componente estático

--- react/pensando-en-react/src/App.js
+++ react/pensando-en-react/src/App.js
@@ -2,7 +2,50 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hola mundo</h1>
+    <>
+      <form>
+        <input type="text" placeholder="Buscar..." />
+        <label>
+          <input type="checkbox" />
+          {' '}
+          Mostrar solo productos en stock
+        </label>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th colSpan="2">
+              Fruta
+            </th>
+          </tr>
+          <tr>
+            <td>Manzana</td>
+            <td>1€</td>
+          </tr>
+          <tr>
+            <td>Naranja</td>
+            <td>1€</td>
+          </tr>
+          <tr>
+            <td>
+              <span style={{ color: 'red' }}>
+                Maracuyá
+              </span>
+            </td>
+            <td>
+              2€
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 }
 
